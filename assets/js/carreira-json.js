fba79b9035472fc1e519b6bdbaac65f9ec5bf83d
Giovanna Karolline <mennezes.giovanna@gmail.com>
2026-06-19 17:34:30 -0300

    velocidade das animaçôes e troca de svg

--- assets/js/carreira-json.js
+++ assets/js/carreira-json.js
@@ -61,6 +61,9 @@ var ICONS = {
   star:
     '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" width="26" height="26" aria-hidden="true">' +
     '<path d="M12 17.27L18.18 21l-1.64-7.03L22 9.24l-7.19-.61L12 2 9.19 8.63 2 9.24l5.46 4.73L5.82 21z"/></svg>',
+  laptop:
+    '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" width="28" height="28" aria-hidden="true">' +
+    '<path d="M20 18c1.1 0 1.99-.9 1.99-2L22 6c0-1.1-.9-2-2-2H4c-1.1 0-2 .9-2 2v10c0 1.1.9 2 2 2H0v2h24v-2h-4zM4 6h16v10H4V6z"/></svg>',
 };
 
 
@@ -77,6 +80,7 @@ var EMOJI_MAP = {
   '\uD83C\uDF31': 'seedling',
   '\uD83D\uDE80': 'rocket',
   '\u2B50': 'star',
+  '\uD83D\uDCBB': 'laptop',
 };
 
 
